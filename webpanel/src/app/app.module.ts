e5c12417fb45586f86f8e1d60f72fff123aac4b2
Tóth Dániel <novalisdenahi@gmail.com>
2024-10-11 15:36:07 +0200

    Security oct 2024 (#35)
    * npm audit fix
    
    * ng update to 18.2.7
    
    * updated configcat depenceies
    
    * 2.0.4
    
    * fix browser output path

--- webpanel/src/app/app.module.ts
+++ webpanel/src/app/app.module.ts
@@ -6,31 +6,23 @@ import { NgConfigCatPublicApiUIModule } from 'ng-configcat-publicapi-ui';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppData } from './app-data';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
-    AppRoutingModule,
-    HttpClientModule,
-    NgConfigCatPublicApiUIModule.forRoot(() => ({
-      basePath: (window as any).CONFIGCAT_APPDATA.publicApiBaseUrl,
-      dashboardBasePath: (window as any).CONFIGCAT_APPDATA.dashboardBasePath,
-      basicAuthUsername: (window as any).CONFIGCAT_APPDATA.basicAuthUsername,
-      basicAuthPassword: (window as any).CONFIGCAT_APPDATA.basicAuthPassword
-    }))
-  ],
-  providers: [AppData,
-    {
-      provide: AppData,
-      useValue: (window as any).CONFIGCAT_APPDATA
-    }
-  ],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
+        AppRoutingModule,
+        NgConfigCatPublicApiUIModule.forRoot(() => ({
+            basePath: (window as any).CONFIGCAT_APPDATA.publicApiBaseUrl,
+            dashboardBasePath: (window as any).CONFIGCAT_APPDATA.dashboardBasePath,
+            basicAuthUsername: (window as any).CONFIGCAT_APPDATA.basicAuthUsername,
+            basicAuthPassword: (window as any).CONFIGCAT_APPDATA.basicAuthPassword
+        }))], providers: [AppData,
+        {
+            provide: AppData,
+            useValue: (window as any).CONFIGCAT_APPDATA
+        }, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
