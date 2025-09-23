1f0042efab384bfde440d0c3cac65571492db3ca
novalisdenahi <novalisdenahi@gmail.com>
2025-09-23 14:10:02 +0200

    Remove js files

--- webpanel/src/environments/environment.prod.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.environment = void 0;
-exports.environment = {
-    production: true
-};
-//# sourceMappingURL=environment.prod.js.map
