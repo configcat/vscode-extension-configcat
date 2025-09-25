7d042a25cec8b85a1ebabb5646a5348d770c07ee
novalisdenahi <novalisdenahi@gmail.com>
2025-09-25 21:42:00 +0200

    Remove routes

--- webpanel/src/app/app.config.ts
+++ webpanel/src/app/app.config.ts
@@ -2,15 +2,8 @@ import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http"
 import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from "@angular/core";
 import { MatNativeDateModule } from "@angular/material/core";
 import { MatDialogModule } from "@angular/material/dialog";
-import {
-  provideRouter,
-  withComponentInputBinding,
-  withInMemoryScrolling,
-  withRouterConfig,
-} from "@angular/router";
 import { CONFIGCAT_PUBLICAPI_UI_CONFIGURATION, provideConfigCatPublicApiUi } from "ng-configcat-publicapi-ui";
 import { AppData } from "./app-data";
-import { routes } from "./app-routing.module";
 
 declare global {
   interface Window {
@@ -25,12 +18,6 @@ export const appConfig: ApplicationConfig = {
       MatNativeDateModule
     ),
     provideZoneChangeDetection({ eventCoalescing: true, runCoalescing: true }),
-    provideRouter(
-      routes,
-      withInMemoryScrolling({ scrollPositionRestoration: "enabled", anchorScrolling: "enabled" }),
-      withComponentInputBinding(),
-      withRouterConfig({ paramsInheritanceStrategy: "always" })
-    ),
     {
       provide: AppData,
       useValue: window["CONFIGCAT_APPDATA"],
