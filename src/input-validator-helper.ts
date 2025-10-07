8406fd8f550a029779e00cc661281d9e93183174
novalisdenahi <novalisdenahi@gmail.com>
2025-10-07 18:09:23 +0200

    Remove white space

--- src/input-validator-helper.ts
+++ src/input-validator-helper.ts
@@ -9,5 +9,5 @@ export function lengthValidator(value: string): string | null {
   if (value.length < 255) {
     return null;
   }
-  return " The field must be at max 255 characters long.";
+  return "The field must be at max 255 characters long.";
 }
