aea5228229c95024efafb7a62a3985a1624f520f
Tóth Dániel <novalisdenahi@gmail.com>
2026-04-08 16:56:05 +0200

    Config create fix & Security fix (#54)
    * Use correct productId and fix setting fields order
    
    * update dependecies
    
    * change back until ng update
    
    * ng update
    
    * update configcat dependecies back to latest
    
    * npm audit fix
    
    * npm audit fix

--- src/settings/setting-provider.ts
+++ src/settings/setting-provider.ts
@@ -88,8 +88,8 @@ export class SettingProvider implements vscode.TreeDataProvider<Resource> {
 
         const settingsService = this.publicApiService.createSettingsService(publicApiConfiguration, workspaceConfiguration.publicApiBaseUrl);
         return settingsService.getSettings(workspaceConfiguration.configId).then(settings => {
-          const items = settings.data.map((s) => new Resource(String(s.settingId), s.key ?? "",
-            s.name ?? "", s.hint ?? "",
+          const items = settings.data.map((s) => new Resource(String(s.settingId), s.name ?? "",
+            s.key ?? "", s.hint ?? "",
             vscode.TreeItemCollapsibleState.None));
           statusBar.hide();
           if (this.selectSettingAfterRefresh) {
